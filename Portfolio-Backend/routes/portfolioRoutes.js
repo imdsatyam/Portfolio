540a74942a26e5498fa1565511b836c52a97ddf2
unknown <imdsatyam@gmail.com>
2025-01-23 13:46:51 +0530

    Final Code For Portfolio

--- Portfolio-Backend/routes/portfolioRoutes.js
+++ Portfolio-Backend/routes/portfolioRoutes.js
@@ -3,6 +3,5 @@ const { sendEmail } = require('../controllers/emailController');
 const router = express.Router();
 
 router.post('/send-email', sendEmail);
-router.get('/hello', (req, res) => res.send("Hello fox"));
 
 module.exports = router;
