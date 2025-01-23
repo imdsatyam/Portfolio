b97fa773e78eed3ef9d981dd4704755686bd23d9
unknown <imdsatyam@gmail.com>
2025-01-23 12:57:52 +0530

    Changed Packeage file

--- Portfolio-Backend/routes/portfolioRoutes.js
+++ Portfolio-Backend/routes/portfolioRoutes.js
@@ -3,5 +3,6 @@ const { sendEmail } = require('../controllers/emailController');
 const router = express.Router();
 
 router.post('/send-email', sendEmail);
+router.get('/hello', (req, res) => res.send("Hello fox"));
 
 module.exports = router;
